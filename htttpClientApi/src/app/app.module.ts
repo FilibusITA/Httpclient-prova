9208a064dbf23a7924c8391ca699df2eab895607
FilibusITA <filibertopuntoricci@gmail.com>
2021-09-14 16:42:08 +0200

    errore 422 v2

--- htttpClientApi/src/app/app.module.ts
+++ htttpClientApi/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SkillComponent } from './skill/skill.component';
 import { CategoriesComponent } from './categories/categories.component'
-//import {RatingModule} from 'primeng/rating';
+import {RatingModule} from 'primeng/rating';
 import {SplitButtonModule} from 'primeng/splitbutton';
 import {TabViewModule} from 'primeng/tabview';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RatingModule } from 'ngx-bootstrap/rating';
+//import { RatingModule } from 'ngx-bootstrap/rating';
 import { HomeComponent } from './home/home.component';
 import { BassoComponent } from './basso/basso.component';
 import { SchermataHomeComponent } from './schermata-home/schermata-home.component';
